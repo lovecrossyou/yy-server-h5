4bae11c0eb9b95e17b8ff9688642424539de9bf5
lovecross <lovecrossyoutt@hotmail.com>
2018-12-19 21:30:56 +0800

    xx

--- yy_app.js
+++ yy_app.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var compression = require('compression');
 // Create our Express application
-var app = express();
+var yy_app = express();
 var proxys = require('./src/proxy/proxys')
 var http = require('http');
 
-app.use(compression());
-app.disable('x-powered-by');
+yy_app.use(compression());
+yy_app.disable('x-powered-by');
 var ejs = require('ejs');  //我是新引入的ejs插件
-app.set('views', './views'); // 指定视图所在的位置
+yy_app.set('views', './views'); // 指定视图所在的位置
 //
-app.engine('html', ejs.__express);
-app.set('view engine', 'html');
-app.use('/yy', express.static(__dirname + "/views/"));
-app.use('/h5', proxys);
+yy_app.engine('html', ejs.__express);
+yy_app.set('view engine', 'html');
+yy_app.use('/yy', express.static(__dirname + "/views/"));
+yy_app.use('/h5', proxys);
 
-var server = http.createServer(app).listen(7003, function () {
+var server = http.createServer(yy_app).listen(7003, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('ranning at host', host);
